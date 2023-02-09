db675d96098e50ebb1243677f384082f142e27b7
yichienlu <yichien.lu89@gmail.com>
2023-02-09 22:15:22 +0800

    20230209-03

--- Vue_Week5/cart.js
+++ Vue_Week5/cart.js
@@ -1,10 +1,28 @@
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.47/vue.esm-browser.min.js'
 
 const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
-apiPath = 'yclu'
+const apiPath = 'yclu'
 
 const app = createApp({
-
+  data(){
+    return {
+      products:[]
+    }
+  },
+  methods:{
+    getProducts(){
+      axios.get(`${apiUrl}/api/${apiPath}/products/all`)
+        .then((res)=>{
+          console.log(res.data)
+        })
+        .catch((err)=>{
+          console.dir(err)
+        })
+    }
+  },
+  mounted(){
+    this.getProducts()
+  }
 });
 
 app.mount('#app')
