a8936b34ebcccb50b6a3541580d84322de95e298
yichienlu <yichien.lu89@gmail.com>
2023-02-10 14:38:55 +0800

    20230210 點選跳出modal

--- Vue_Week5/cart.js
+++ Vue_Week5/cart.js
@@ -3,22 +3,59 @@ import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.47/vue
 const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
 const apiPath = 'yclu'
 
+const productModal = {
+  props:['id'],
+  data(){
+    return {
+      modal:{},
+      tempProduct:{}
+    }
+  },
+  template: '#userProductModal',
+  watch:{
+    id(){
+      console.log(this.id)
+      axios.get(`${apiUrl}/api/${apiPath}/product/${this.id}`)
+      .then((res)=>{
+        console.log(res.data.product)
+        this.tempProduct = res.data.product;
+        this.modal.show();
+      })
+    }
+  },
+  mounted(){
+    this.modal = new bootstrap.Modal(this.$refs.modal);
+    // this.modal.show()
+  }
+}
+
+
+
 const app = createApp({
   data(){
     return {
-      products:[]
+      products:[],
+      productId:''
     }
   },
   methods:{
     getProducts(){
       axios.get(`${apiUrl}/api/${apiPath}/products/all`)
       .then((res)=>{
-        console.log(res.data)
+        console.log('產品列表：' , res.data.products[0]);
+        this.products = res.data.products;
       })
       .catch((err)=>{
         console.log(err)
       })
+    },
+    openModal(id){
+      this.productId = id
+      console.log(this.productId)
     }
+  },
+  components:{
+    productModal
   },
   mounted(){
     this.getProducts()
