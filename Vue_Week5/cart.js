8b1566d07ca67390a2d946ec68432b4eec00cfce
yichienlu <yichien.lu89@gmail.com>
2023-02-10 17:51:15 +0800

    20220210 完成加入購物車

--- Vue_Week5/cart.js
+++ Vue_Week5/cart.js
@@ -4,7 +4,7 @@ const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
 const apiPath = 'yclu'
 
 const productModal = {
-  props:['id'],
+  props:['id', 'addToCart'],
   data(){
     return {
       modal:{},
@@ -21,6 +21,11 @@ const productModal = {
         this.tempProduct = res.data.product;
         this.modal.show();
       })
+    }
+  },
+  methods:{
+    hide(){
+      this.modal.hide();
     }
   },
   mounted(){
@@ -52,6 +57,17 @@ const app = createApp({
     openModal(id){
       this.productId = id
       console.log(this.productId)
+    },
+    addToCart(product_id, qty=1){
+      const data = {
+        product_id,
+        qty
+      };
+      axios.post(`${apiUrl}/api/${apiPath}/cart`,{data})
+      .then((res)=>{
+        console.log('加入購物車', res.data);
+        this.$refs.productModal.hide();
+      })
     }
   },
   components:{
